3cc3cc4d70c5c6e03ed5e60986f2e68ccf21e069
Bruno Kenji <bruno.168899-2024@aluno.unicv.edu.br>
2024-11-25 23:36:43 -0300

    .

--- cadastrarmovi.c
+++ cadastrarmovi.c
@@ -1,11 +1,8 @@
-
-
-
-
 #include "funcoes.h"
 
-void casdastrarmovi(){
-  movimentacao mv;
+void casdastrarmovi(conta_bancaria *conta){
+
+ movimentacao mv;
 
     gotoxy(46, 15);
     fflush(stdin);
@@ -22,11 +19,35 @@ void casdastrarmovi(){
     gotoxy(46, 18);
     scanf("%.2f", &mv.vl_movimento);
 
-      double novoSaldo = mv.vl_saldo - mv.vl_movimento;
+      if (strcmp(mv.tp_movimentacao, "Debito")==0){
+        if ((conta->vl_saldo - mv.vl_movimento) < -conta->vl_limite)
+        {
+          gotoxy(11,24);
+          printf("Saldo insuficiente0");
+          return;
+        }
+        conta->vl_saldo -= mv.vl_movimento; 
+      }else if (strcmp(mv.tp_movimentacao, "Credito")==0)
+      {
+        double total_disponivel = conta->vl_saldo + conta->vl_limite;
+      if (mv.vl_movimento > total_disponivel)
+      {
+        gotoxy(11,24 );
+        printf("Saldo e credito insuficiente");
+        return;
+      
+      }
+      conta ->vl_saldo += mv.vl_movimento;
+      }else{
+        printf("Tipo de Movimentação invalida");
+        return;
+      }
+      
+    mv.vl_saldo = conta->vl_saldo;
+    mv.codigo_conta= conta->codigo_conta;
+      gotoxy(46,19);
+      printf("%.2f", conta->vl_saldo);
 
-
-    gotoxy(46, 19);
-    printf("%.2f", novoSaldo);
-
+    
 
 }
